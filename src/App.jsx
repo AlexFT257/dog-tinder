326671cfc0ef66d52d5bb830c0e92c0e1e749d3b
AlexFT257 <ftb2570@gmail.com>
2023-04-13 22:19:27 -0400

    cambios en el frontend post merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,184 +1,216 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import { Box, Grid, Card, CardContent, CardMedia, Typography, List, ListItem, TextField, Button, ImageList } from "@mui/material";
+import {
+  Box,
+  Grid,
+  Card,
+  CardContent,
+  CardMedia,
+  CardActionArea,
+  CardActions,
+  Typography,
+  List,
+  ListItem,
+  TextField,
+  Button,
+  ImageList,
+} from "@mui/material";
 import axios from "axios";
-import dogNames from "dog-names"
-
+import dogNames from "dog-names";
 
 function App() {
   const [listaRechazados, setListaRechazados] = useState([]);
   const [listaAceptados, setListaAceptados] = useState([]);
-  const [perroActual, setPerroActual] = useState({ name: '', image: '' });
+  const [perroActual, setPerroActual] = useState({ name: "", image: "" });
 
   useEffect(() => {
     buscarImagenPerro();
-  }, [])
+  }, []);
 
   const buscarImagenPerro = () => {
-    axios.get('https://dog.ceo/api/breeds/image/random').then((response) => {
-      setPerroActual({ name: dogNames.allRandom(), image: response.data.message })
-    })
-  }
+    axios.get("https://dog.ceo/api/breeds/image/random").then((response) => {
+      setPerroActual({
+        name: dogNames.allRandom(),
+        image: response.data.message,
+      });
+    });
+  };
 
   const aceptarPerro = (perroActual) => {
     setListaAceptados((listaAceptados) => [...listaAceptados, perroActual]);
     buscarImagenPerro();
   };
 
+  // const randomDogs = () => {
+  //   fetch("https://dog.ceo/api/breeds/image/random")
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       console.log(data);
+
+  //       setPerroActual({ name: generateName(), image: data.message });
+  //     });
+  // };
+
+  // useEffect(() => {
+  //   randomDogs();
+  // }, []);
+
   const rechazarPerro = (perroActual) => {
-    setListaRechazados((listaRechazados) => [...listaRechazados, perroActual]);
+    console.log("rechazado");
+    console.log(perroActual);
+    setListaRechazados([...listaRechazados, perroActual]);
+    // randomDogs();
     buscarImagenPerro();
   };
 
-
   return (
-    <Box justifyContent="space-between">
-      <Grid container spacing={0} direction="row" justifyContent="space-between" alignItems="stretch" >
-        <Grid item xs={4} direction="row" sx={{ background: "red" }}>
-          <ImageList rowHeight={100} cols={1} >
+      <Grid
+        container
+        spacing={2}
+        direction="row"
+        justifyContent="space-between"
+        alignItems="stretch"
+      >
+        {/* lista rechazados */}
+        <Grid item xs={4}  direction="column" sx={{ }}>
+          <List rowHeight={100} cols={1} style={{}}>
             {listaRechazados.map((item) => (
-              <Card xs={4} direction="column" key={item.name} >
-                <CardMedia component="img" style={{ width: "100%", height: "100%", objectFit: "contain" }} loading="lazy" image={item.image} />
-                <CardContent>
-                  <Typography variant="h5" component="div">
-                    {item.name}
-                  </Typography>
-                </CardContent>
-              </Card>
+              <ListItem>
+                <Card direction="column" key={item.name}>
+                  <CardMedia
+                    component="img"
+                    style={{
+                      width: "",
+                      height: "",
+                      objectFit: "cover",
+                    }}
+                    loading="lazy"
+                    image={item.image}
+                  />
+                  <CardContent>
+                    <Typography variant="h5" component="div">
+                      {console.log(item.name)}
+                      {item.name}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </ListItem>
             ))}
-          </ImageList>
+          </List>
         </Grid>
-        <Grid item xs={4} direction="row" sx={{ background: "gray" }}>
-          <Card xs={4} direction="column" >
-            <CardMedia component="img" style={{ width: "100%", height: "200px", objectFit: "contain" }} loading="lazy" image={perroActual.image} />
-            <CardContent>
-              <Typography variant="h5" component="div">
-                {perroActual.name}
-              </Typography>
-            </CardContent>
-            <Button variant="contained" onClick={() => rechazarPerro(perroActual)}>Rechazar</Button>
-            <Button variant="contained" onClick={() => aceptarPerro(perroActual)}>Aceptar</Button>
+        <Grid item xs={4} direction="column" >
+          <Card >
+            
+              <CardMedia
+                component="img"
+                width="500px"
+                height="250px"
+                image={perroActual.image}
+                alt="Contemplative Reptile"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {perroActual.name}
+                </Typography>
+              </CardContent>
+            <CardActions>
+              <Button onClick={()=>rechazarPerro(perroActual)} size="small" color="primary">
+                Rechazar
+              </Button>
+              <Button onClick={()=>aceptarPerro(perroActual)} size="small" color="primary">
+                Aceptar
+              </Button>
+            </CardActions>
           </Card>
         </Grid>
-        <Grid item xs={4} direction="row" sx={{ background: "green" }}>
-          <List>
+        <Grid item xs={4} direction="row" style={{alignItems: "center"}} >
+          <List style={{alignItems: "center"}}>
             {listaAceptados.map((item) => (
-              <Card xs={4} direction="column" key={item.name} >
-                <CardMedia component="img" style={{ width: "100%", height: "100%", objectFit: "contain" }} loading="lazy" image={item.image} />
-                <CardContent>
-                  <Typography variant="h5" component="div">
-                    {item.name}
-                  </Typography>
-                </CardContent>
-              </Card>
+              <ListItem>
+                <Card direction="column"  key={item.name}>
+                  <CardMedia
+                    component="img"
+                    style={{
+                      maxWidth: "500px",
+                      maxHeight: "250px",
+                      objectFit: "contain",
+                    }}
+                    loading="lazy"
+                    image={item.image}
+                  />
+                  <CardContent>
+                    <Typography variant="h5" component="div">
+                      {item.name}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </ListItem>
             ))}
           </List>
         </Grid>
       </Grid>
-    </Box>
-  )
-  const [count, setCount] = useState(0);
+  );
 
-  const dog = {
-    nombre: "Firulais",
-    foto: "https://www.royalcanin.es/~/media/Royal-Canin/Product-Categories/dog-adult-landing-hero.ashx",
-  };
+ 
 
-  const [listaRechazados, setListaRechazados] = useState([]);
-  const [listaAceptados, setListaAceptados] = useState([dog]);
-  const [perroActual, setPerroActual] = useState(dog);
-
-
-  function capFirst(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-  }
-
-  function getRandomInt(min, max) {
-  	return Math.floor(Math.random() * (max - min)) + min;
-  }
-
-function generateName(){
-	var name1 = ["abandoned","self-reliant","selfish","sentimental","separate","serene","serious","serpentine","several","severe","shabby","shadowy","shady","shallow","shameful","shameless","sharp","shimmering","shiny","shocked","shocking","shoddy","short","short-term","showy","shrill","shy","sick","silent","silky","silly","silver","similar","simple","simplistic","sinful","single","sizzling","skeletal","skinny","sleepy","slight","slim","slimy","slippery","slow","slushy","small","smart","smoggy","smooth","smug","snappy","snarling","sneaky","sniveling","snoopy","sociable","soft","soggy","solid","somber","some","spherical","sophisticated","sore","sorrowful","soulful","soupy","sour","Spanish","sparkling","sparse","specific","spectacular","speedy","spicy","spiffy","spirited","spiteful","splendid","spotless","spotted","spry","square","squeaky","squiggly","stable","staid","stained","stale","standard","starchy","stark","starry","steep","sticky","stiff","stimulating","stingy","stormy","straight","strange","steel","strict","strident","striking","striped","strong","studious","stunning","stupendous","stupid","sturdy","stylish","subdued","submissive","substantial","subtle","suburban","sudden","sugary","sunny","super","superb","superficial","superior","supportive","sure-footed","surprised","suspicious","svelte","sweaty","sweet","sweltering","swift","sympathetic","tall","talkative","tame","tan","tangible","tart","tasty","tattered","taut","tedious","teeming","tempting","tender","tense","tepid","terrible","terrific","testy","thankful","that","these","thick","thin","third","thirsty","this","thorough","thorny","those","thoughtful","threadbare","thrifty","thunderous","tidy","tight","timely","tinted","tiny","tired","torn","total","tough","traumatic","treasured","tremendous","tragic","trained","tremendous","triangular","tricky","trifling","trim","trivial","troubled","true","trusting","trustworthy","trusty","truthful","tubby","turbulent","twin","ugly","ultimate","unacceptable","unaware","uncomfortable","uncommon","unconscious","understated","unequaled","uneven","unfinished","unfit","unfolded","unfortunate","unhappy","unhealthy","uniform","unimportant","unique","united","unkempt","unknown","unlawful","unlined","unlucky","unnatural","unpleasant","unrealistic","unripe","unruly","unselfish","unsightly","unsteady","unsung","untidy","untimely","untried","untrue","unused","unusual","unwelcome","unwieldy","unwilling","unwitting","unwritten","upbeat","upright","upset","urban","usable","used","useful","useless","utilized","utter","vacant","vague","vain","valid","valuable","vapid","variable","vast","velvety","venerated","vengeful","verifiable","vibrant","vicious","victorious","vigilant","vigorous","villainous","violet","violent","virtual","virtuous","visible","vital","vivacious","vivid","voluminous","wan","warlike","warm","warmhearted","warped","wary","wasteful","watchful","waterlogged","watery","wavy","wealthy","weak","weary","webbed","wee","weekly","weepy","weighty","weird","welcome","well-documented","well-groomed","well-informed","well-lit","well-made","well-off","well-to-do","well-worn","wet","which","whimsical","whirlwind","whispered","white","whole","whopping","wicked","wide","wide-eyed","wiggly","wild","willing","wilted","winding","windy","winged","wiry","wise","witty","wobbly","woeful","wonderful","wooden","woozy","wordy","worldly","worn","worried","worrisome","worse","worst","worthless","worthwhile","worthy","wrathful","wretched","writhing","wrong","wry","yawning","yearly","yellow","yellowish","young","youthful","yummy","zany","zealous","zesty","zigzag","rocky"];
-	var name2 = ["people","neck","pension","plate","purple","ruin","ship","skirt","slice","snow","specialist","stroke","switch","trash","tune","zone","anger","award","bid","bitter","boot","bug","camp","candy","carpet","cat","champion","channel","clock","comfort","cow","crack","engineer","entrance","fault","grass","guy","hell","highlight","incident","island","joke","jury","leg","lip","mate","motor","nerve","passage","pen","pride","priest","prize","promise","resident","resort","ring","roof","rope","sail","scheme","script","sock","station","toe","tower","truck","witness","can","will","other","use","make","good","look","help","go","great","being","still","public","read","keep","start","give","human","local","general","specific","long","play","feel","high","put","common","set","change","simple","past","big","possible","particular","major","personal","current","national","cut","natural","physical","show","try","check","second","call","move","pay","let","increase","single","individual","turn","ask","buy","guard","hold","main","offer","potential","professional","international","travel","cook","alternative","special","working","whole","dance","excuse","cold","commercial","low","purchase","deal","primary","worth","fall","necessary","positive","produce","search","present","spend","talk","creative","tell","cost","drive","green","support","glad","remove","return","run","complex","due","effective","middle","regular","reserve","independent","leave","original","reach","rest","serve","watch","beautiful","charge","active","break","negative","safe","stay","visit","visual","affect","cover","report","rise","walk","white","junior","pick","unique","classic","final","lift","mix","private","stop","teach","western","concern","familiar","fly","official","broad","comfortable","gain","rich","save","stand","young","heavy","lead","listen","valuable","worry","handle","leading","meet","release","sell","finish","normal","press","ride","secret","spread","spring","tough","wait","brown","deep","display","flow","hit","objective","shoot","touch","cancel","chemical","cry","dump","extreme","push","conflict","eat","fill","formal","jump","kick","opposite","pass","pitch","remote","total","treat","vast","abuse","beat","burn","deposit","print","raise","sleep","somewhere","advance","consist","dark","double","draw","equal","fix","hire","internal","join","kill","sensitive","tap","win","attack","claim","constant","drag","drink","guess","minor","pull","raw","soft","solid","wear","weird","wonder","annual","count","dead","doubt","feed","forever","impress","repeat","round","sing","slide","strip","wish","combine","command","dig","divide","equivalent","hang","hunt","initial","march","mention","spiritual","survey","tie","adult","brief","crazy","escape","gather","hate","prior","repair","rough","sad","scratch","sick","strike","employ","external","hurt","illegal","laugh","lay","mobile","nasty","ordinary","respond","royal","senior","split","strain","struggle","swim","train","upper","wash","yellow","convert","crash","dependent","fold","funny","grab","hide","miss","permit","quote","recover","resolve","roll","sink","slip","spare","suspect","sweet","swing","twist","upstairs","usual","abroad","brave","calm","concentrate","estimate","grand","male","mine","prompt","quiet","refuse","regret","reveal","rush","shake","shift","shine","steal","suck","surround","bear","brilliant","dare","dear","delay","drunk","female","hurry","inevitable","invite","kiss","neat","pop","punch","quit","reply","representative","resist","rip","rub","silly","smile","spell","stretch","stupid","tear","temporary","tomorrow","wake","wrap","yesterday","Thomas","Tom","Lieuwe"];
-	var name = capFirst(name1[getRandomInt(0, name1.length + 1)]) + ' ' + capFirst(name2[getRandomInt(0, name2.length + 1)]);
-	return name;
-}
-
-  const randomDogs = () => {
-    fetch("https://dog.ceo/api/breeds/image/random")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        
-        setPerroActual({ nombre: generateName(), foto: data.message });
-      });
-  };
-
-  useEffect(() => {
-    randomDogs();
-  }, []);
-
-  const rechazarPerro = () => {
-    console.log("rechazado");
-    setListaRechazados([...listaRechazados, perroActual]);
-    randomDogs();
-  };
-
-
-  return (<>
-    <Grid container direction="row" justifyContent={""} spacing={12}  >
-      {/* lista de rechazados */}
-      <Grid item style={{backgroundColor: "white"}}  xs={3}>
-        <List>{listaRechazados.map((item)=>{
-          return <ListItem>
-            <Card>
-              <CardActionArea>
-                <CardMedia
-                  component="img"
-                  height="140"
-                  image={item.foto}
-                  alt="Contemplative Reptile"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    {item.nombre}
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-              </Card>
-          </ListItem>
-        })}</List>
+  return (
+    <>
+      <Grid container direction="row" justifyContent={""} spacing={12}>
+        {/* lista de rechazados */}
+        <Grid item style={{ backgroundColor: "white" }} xs={3}>
+          <List>
+            {listaRechazados.map((item) => {
+              return (
+                <ListItem>
+                  <Card>
+                    <CardActionArea>
+                      <CardMedia
+                        component="img"
+                        height="140"
+                        image={item.foto}
+                        alt="Contemplative Reptile"
+                      />
+                      <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                          {item.nombre}
+                        </Typography>
+                      </CardContent>
+                    </CardActionArea>
+                  </Card>
+                </ListItem>
+              );
+            })}
+          </List>
+        </Grid>
+        {/* perro actual */}
+        <Grid item md={6} xs={6}>
+          <Card>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                height="140"
+                image={perroActual.foto}
+                alt="Contemplative Reptile"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {perroActual.nombre}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+              <Button onClick={rechazarPerro} size="small" color="primary">
+                Rechazar
+              </Button>
+              <Button onClick={randomDogs} size="small" color="primary">
+                Aceptar
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
+        <Grid item md={3} xs={3}></Grid>
       </Grid>
-      {/* perro actual */}
-      <Grid item md={6}  xs={6}>
-        <Card>
-          <CardActionArea>
-            <CardMedia
-              component="img"
-              height="140"
-              image={perroActual.foto}
-              alt="Contemplative Reptile"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                {perroActual.nombre}
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-          <CardActions>
-            <Button onClick={rechazarPerro} size="small" color="primary">
-              Rechazar
-            </Button>
-            <Button onClick={randomDogs} size="small" color="primary">
-              Aceptar
-            </Button>
-          </CardActions>
-        </Card>
-      </Grid>
-      <Grid item md={3}  xs={3}>
-      </Grid>
-        
-    </Grid></>
+    </>
   );
 }
 
